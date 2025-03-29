dc872fa3f575da30460e359d026067b0638b4578
XTtriple <maynorb0628@gmail.com>
2025-03-29 14:58:27 -0400

    Finished Portfolio

---
+++ script.js
@@ -0,0 +1,9 @@
+// Add your JavaScript code here
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu-links");
+    const icon = document.querySelector(".hamburger-icon");
+    menu.classList.toggle("open");
+    icon.classList.toggle("open");
+    menu.classList.toggle("active");
+}
